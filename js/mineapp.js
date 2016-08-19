bffdb33361c53d7b59db14eb8a1a17346703662c
liulimei <mumeiliuli@163.com>
2016-08-19 16:46:30 +0800

    ionic初始提交
    Signed-off-by: liulimei <mumeiliuli@163.com>

---
+++ js/mineapp.js
@@ -0,0 +1,31 @@
+﻿angular.module('mine', ['ionic'])
+.controller('MineCtrl', function ($scope) {
+    $scope.list = new Array();
+    $scope.changeGreade = function (index) {
+        var rows = 0,cols=0,mineNum=0;
+        switch (index) {
+            case 1:
+                rows = 8;
+                cols = 8;
+                mineNum = 20;
+                break;
+            case 2:
+                rows = 10;
+                cols = 10;
+                mineNum = 40;
+                break;
+            case 3:
+                rows = 16;
+                cols = 10;
+                mineNum = 60;
+                break;
+        }
+        for (var r = 0; r < rows; r++) {
+            for (var c = 0; c < cols; c++) {
+                $scope.list[r][c] = 0;
+            }
+        }
+
+    }
+
+})
---
+++ js/myapp.js
@@ -0,0 +1,31 @@
+﻿angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function ($scope, $ionicModal) {
+    $scope.tasks = [
+      { title: 'webapp教程' },
+      { title: '菜鸟教程' }
+    ];
+    // 创建并载入模型
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+    // 打开新增的模型
+    $scope.newTask = function () {
+        $scope.taskModal.show();
+    };
+    // 表单提交时调用
+    $scope.createTask = function (task) {
+        $scope.tasks.push({
+            title: task.title
+        });
+        $scope.taskModal.hide();
+        task.title = "";
+    };
+    // 关闭新增的模型
+    $scope.closeNewTask = function () {
+        $scope.taskModal.hide();
+    };
+});
